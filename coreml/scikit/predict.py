e4020019364e6a6380ca49f8a02d60d18bbfd51d
dcmartin <github@dcmartin.com>
2020-09-15 11:37:42 -0700

    lgtm

--- coreml/scikit/predict.py
+++
@@ -1,8 +0,0 @@
-import coremltools
-
-model =  coremltools.models.MLModel('houses.mlmodel')
-
-# Make predictions
-predictions = model.predict({'BDRMS': 1.0, 'BTRMS': 1.0, 'FMR': 1240})
-
-print predictions
